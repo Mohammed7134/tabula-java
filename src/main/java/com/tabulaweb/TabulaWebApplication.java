22a328f8415057ed6f5c33bc04968968b70f881b
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2026-05-18 10:30:40 +0300

    solved maximum upload size exceeded issue

--- src/main/java/com/tabulaweb/TabulaWebApplication.java
+++ src/main/java/com/tabulaweb/TabulaWebApplication.java
@@ -2,6 +2,11 @@ package com.tabulaweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
+
+import jakarta.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 public class TabulaWebApplication {
@@ -9,5 +14,16 @@ public class TabulaWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(TabulaWebApplication.class, args);
     }
-
+ @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        
+        // حدد حجم الملف الواحد الأقصى (مثال: 50 ميجابايت)
+        factory.setMaxFileSize(DataSize.ofMegabytes(50));
+        
+        // حدد حجم الطلب الكلي الأقصى للملفات الأربعة معاً (مثال: 200 ميجابايت)
+        factory.setMaxRequestSize(DataSize.ofMegabytes(200));
+        
+        return factory.createMultipartConfig();
+    }
 }
